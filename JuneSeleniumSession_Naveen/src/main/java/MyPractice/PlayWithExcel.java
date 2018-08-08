68640d3b11e23b452af93991cadb2a8cd8134fe1
u332678 <u332678@L5CG535081R.gac.gulfaero.com>
2018-08-08 09:34:49 -0400

    Modified excel file path in work machine

--- JuneSeleniumSession_Naveen/src/main/java/MyPractice/PlayWithExcel.java
+++ JuneSeleniumSession_Naveen/src/main/java/MyPractice/PlayWithExcel.java
@@ -7,9 +7,12 @@ public class PlayWithExcel {
 	public static void main(String[] args) {
 
 
-			Xls_Reader reader = new Xls_Reader("C:\\Users\\Madhu X1_Tab\\Downloads\\Compressed\\"
-					+ "JuneSeleniumSessions-master\\src\\main\\java\\testdata\\TestData.xlsx");
-			
+			//Xls_Reader reader = new Xls_Reader("C:\\Users\\Madhu X1_Tab\\Downloads\\Compressed\\"
+			//		+ "JuneSeleniumSessions-master\\src\\main\\java\\testdata\\TestData.xlsx");
+		// for Work Computer
+		Xls_Reader reader = new Xls_Reader("L:\\git\\WorkBeforePOM\\JuneSeleniumSession_Naveen\\src\\main\\"
+				+ "java\\testdata\\FreeCrmTestData.xlsx");	
+		
 			String sheetName = "login";
 			
 			int myRows = reader.getRowCount(sheetName);
